bf648d5c8c041e5a50f9ea29a9a7592a253b7762
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-11-26 08:02:32 +0530

    Ts Arrays

---
+++ TYPESCRIPT/Intro/myArrays.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fruits = [];
+var fruitCount = [];
+var allUsers = [];
+var MLModels = [
+    [255, 255, 255],
+    []
+];
+console.log(fruits);
+fruits.push("Mango");
+fruitCount.push(3);
---
+++ TYPESCRIPT/Intro/myArrays.ts
@@ -0,0 +1,18 @@
+const fruits:string []=[]
+const fruitCount:Array<number>=[]
+type User ={
+    name:string
+    isActive :boolean
+}
+
+const allUsers :User[]=[]
+
+const MLModels : number [][]=[
+    [255,255,255],
+    []
+]
+console.log(fruits)
+fruits.push("Mango")
+fruitCount.push(3)
+
+export{}
