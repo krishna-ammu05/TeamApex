c4240e05279798b040ff49a4175f6d302ad64669
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-11-26 08:02:48 +0530

    TS Objects

--- TYPESCRIPT/Intro/myObjects.js
+++ TYPESCRIPT/Intro/myObjects.js
@@ -1,11 +1,34 @@
 "use strict";
+// const User = {
+//     name : "krishna",
+//     email: "krish@5",
+//     isActive :true
+// }
+// function createUser (name :string,isPaid :boolean){}
 Object.defineProperty(exports, "__esModule", { value: true });
-var User = {
-    name: "krishna",
-    email: "krish@5",
-    isActive: true
-};
-function createUser(name, isPaid) {
+// Exactly the same as the earlier example
+function printCoord(pt) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
 }
-var newUser = { name: "krish", isPaid: false, email: krish };
+printCoord({ x: 100, y: 100 });
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+var newUser = { name: "krishna", isPaid: false, email: "krishn@.com" };
 createUser(newUser);
+function createCourse() {
+    return { name: "reactjs", price: 400 };
+}
+function create_user(user) {
+    return { name: "", email: "", isActive: true };
+}
+create_user({ name: "", email: "", isActive: true });
+var myUser = {
+    _id: "1234",
+    name: "krish",
+    email: "kris@.com",
+    isActive: false
+};
+myUser.email = "krishn@gmail.com";
+console.log("MyObjects")
--- TYPESCRIPT/Intro/myObjects.ts
+++ TYPESCRIPT/Intro/myObjects.ts
@@ -26,3 +26,58 @@ type Point = {
   printCoord({ x: 100, y: 100 });
 
 
+
+
+function createUser ({name: string,isPaid :boolean}){}
+let newUser = {name:"krishna",isPaid:false,email:"krishn@.com"}
+createUser (newUser)
+
+
+
+function createCourse ():{name :string,price: number}{
+  return {name :"reactjs", price:400}
+}
+
+
+//type Alias
+
+type User ={
+  name:string;
+  email:string;
+  isActive:boolean
+}
+
+
+function create_user (user:User){
+  return {name:"",email:"",isActive:true}
+}
+create_user({name:"",email:"",isActive:true})
+
+
+type user={
+  //readonly is used to read the values and it does not u to modify unique values like id of mysql
+  readonly _id:string
+  name:string
+  email:string
+  isActive :boolean
+  creditcardDetails?:number
+}
+let myUser: user={
+  _id:"1234",
+  name:"krish",
+  email:"kris@.com",
+  isActive:false
+}
+type cardNumber ={
+  cardNo:string
+}
+type cardDate ={
+  cardDate:string
+}
+//defining the new type based the combination of previous two types
+type cardDetails =cardNumber & cardDate &{
+  cvv:number
+}
+myUser.email ="krishn@gmail.com"
+//myUser._id="zhjh"
+export{}
