d93c4a01c65307bab11048c9d2d52f4833976109
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-11-27 22:50:41 +0530

    weather task completed

--- Project/Weather-app/src/App.tsx
+++ Project/Weather-app/src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import { Routes, Route} from 'react-router-dom';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
+import {Home} from './components/Home';
+import { CountryDetails } from './components/CountryDetails';
 function App() {
-  return (
-    <div className="App">
-      Project
-    </div>
+  
+    return (
+      <div className="App">
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/details/:name" element={<CountryDetails />} />
+          </Routes>
+      </div>
+  
   );
 }
 
