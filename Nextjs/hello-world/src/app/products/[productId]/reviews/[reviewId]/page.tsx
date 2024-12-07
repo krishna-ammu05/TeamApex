89db828e0dd8798c9534ca74b7438fdc0703ec8e
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-12-07 20:02:50 +0530

    Error file

--- Nextjs/hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ Nextjs/hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,8 @@
 import { notFound } from "next/navigation";
+
+function getRandomInt(count : number){
+    return Math.floor(Math.random() * count);
+}
 export default function ReviewDetail({
     params,
 }:{
@@ -7,6 +11,11 @@ export default function ReviewDetail({
         reviewId:string;
     };
 }){
+    const random = getRandomInt(2);
+
+    if(random === 1){
+        throw new Error("Error loading review");
+    }
     if(parseInt(params.reviewId)>1000){
         notFound();
     }
