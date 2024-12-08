00fdc9e9fed5f0bbf960716640ba7bdc8ac5798f
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-12-08 19:46:16 +0530

    Handling errors in layouts

--- Nextjs/hello-world/src/app/products/[productId]/layout.tsx
+++ Nextjs/hello-world/src/app/products/[productId]/layout.tsx
@@ -1,8 +1,20 @@
+"use client"
+import { error } from "console";
+import { notFound } from "next/navigation";
+
+function getRandomInt(count : number){
+    return Math.floor(Math.random() * count);
+}
+
 export default function ProductDetailsLayout({
     children,
 }:{
     children:React.ReactNode;
 }){
+    // const random = getRandomInt(2);
+    // if(random===1){
+    //     throw new Error("Error lading product");
+    // }
     return(
         <>
         {children}
