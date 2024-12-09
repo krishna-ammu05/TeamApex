8169f0aa399d85caf0a41afb12e23b0868249fe4
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-12-09 19:10:36 +0530

    Handling Unmatch Routes

--- Nextjs/hello-world/src/app/complex-dashboard/page.tsx
+++ Nextjs/hello-world/src/app/complex-dashboard/page.tsx
@@ -1,26 +1,3 @@
-export default function ComplexDashboardPage({
-    children,
-    users,
-    revenue,
-    notifications
-}:{
-    children : React.ReactNode;
-    users:React.ReactNode;
-    revenue:React.ReactNode;
-    notifications:React.ReactNode;
-}){
-
-        return (
-            <div>
-                <div>{children}</div>
-                <div style={{display:"flex"}}>
-                    <div style={{display: "flex",flexDirection:"column"}}>
-                        <div>{users}</div>
-                        <div>{revenue}</div>
-                    </div>
-                <div style={{display:"flex",flex:1}}>{notifications}</div>
-
-             </div>
-             </div>
-        );
+export default function ComplexDashboardPage(){
+    return <h1>Complex dashboard</h1>
 }
