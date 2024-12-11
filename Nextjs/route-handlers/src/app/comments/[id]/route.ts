79a18ec4dffb0e7abbcaea8008477e9c7837667a
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-12-11 19:34:19 +0530

    Redirects in Route handler

--- Nextjs/route-handlers/src/app/comments/[id]/route.ts
+++ Nextjs/route-handlers/src/app/comments/[id]/route.ts
@@ -1,10 +1,14 @@
 import { comment } from "postcss";
+import { redirect } from "next/navigation";
 import { comments } from "../data";
 
 export async function GET( 
     request:Request,
     {params}:{params:{id:string} }
 ) {
+    if(parseInt(params.id)>comments.length){
+        redirect("/comments");
+    }
     const comment =comments.find(comment=>comment.id ===parseInt(params.id)
 );
     return Response.json(comment)
