20291056768c664b0cf542e4c0341b4ce1dadc41
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-12-11 19:10:44 +0530

    Api DELETE request

--- Nextjs/route-handlers/src/app/comments/[id]/route.ts
+++ Nextjs/route-handlers/src/app/comments/[id]/route.ts
@@ -24,4 +24,16 @@ export async function  PATCH(
     return Response.json(comments[index]);
 
 }
+
+export async function DELETE(
+    request:Request,
+    {params}:{params :{id:string}}
+){
+    const index =comments.findIndex(
+        (comment)=>comment.id===parseInt(params.id)
+    );
+    const deleteComment =comments[index]
+    comments.splice(index ,1)
+    return Response.json(deleteComment)
+}
     
