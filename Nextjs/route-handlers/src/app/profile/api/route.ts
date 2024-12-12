84e9436bcf9737b78a6111f410569821ce653623
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-12-12 22:10:16 +0530

    Headers in Route Handlers

--- Nextjs/route-handlers/src/app/profile/api/route.ts
+++ Nextjs/route-handlers/src/app/profile/api/route.ts
@@ -1,3 +1,12 @@
-export async function GET(){
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request:NextRequest){
+    const requestHeaders = new Headers(request.headers)
+    const headerList = headers()
+    
+    console.log(requestHeaders.get("Authorization"))
+    console.log((await headerList).get("Authorization"));
+
     return new Response("Profile API data");
 }
