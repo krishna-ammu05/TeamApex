2069f0bda250fee880cc1e5664be63809b214c60
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-12-12 22:56:01 +0530

    Cookies in Route Handler

--- Nextjs/route-handlers/src/app/profile/api/route.ts
+++ Nextjs/route-handlers/src/app/profile/api/route.ts
@@ -5,8 +5,13 @@ export async function GET(request:NextRequest){
     const requestHeaders = new Headers(request.headers)
     const headerList = headers()
     
-    console.log(requestHeaders.get("Authorization"))
+    console.log(requestHeaders.get("Authorization"));
     console.log((await headerList).get("Authorization"));
 
-    return new Response("Profile API data");
+    return new Response("<h1>Profile API data</h1>",{
+        headers:{
+            "content-Type":"text/html",
+            "Set-Cookie":"theme=dark"
+        },
+ } );
 }
