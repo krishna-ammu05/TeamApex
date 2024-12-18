d69f2336238d08d56de981352e7362ca6d8f362a
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-12-18 20:43:47 +0530

    updated Navbar

--- Nextjs/Nextjs_Project/basic_portfolio/src/components/layout/Navbar.tsx
+++ Nextjs/Nextjs_Project/basic_portfolio/src/components/layout/Navbar.tsx
@@ -1,4 +1,11 @@
+import MenuItem from "../Navigation/MenuItem"
+
+
 function Navbar(){
-    return <div className="flex justify-center items-center h-10 w-full bg-black text-white">Navbar</div>
+    return <div className="flex justify-center items-center h-10 w-full bg-black text-white">
+        <MenuItem title={"About"}/>
+        <MenuItem title={"Projects"}/>
+        <MenuItem title={"Services"}/>
+    </div>
 }
 export default Navbar
