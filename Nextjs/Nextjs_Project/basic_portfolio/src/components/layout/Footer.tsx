a63cbc5d660eacbddd294f487dcda6b96edc7015
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-12-18 20:42:41 +0530

    Footer added

---
+++ Nextjs/Nextjs_Project/basic_portfolio/src/components/layout/Footer.tsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return <div className="bg-gradient-to-r from-gradientLeft to-gradientRight">
+        <img className="w-full h-44" src="/transitions/transition_gradient.svg" alt="transition"/>
+        <div className="flex justify-center items-center p-4 h-max text-white">Footer</div>
+
+    </div>
+}
+export default Footer;
