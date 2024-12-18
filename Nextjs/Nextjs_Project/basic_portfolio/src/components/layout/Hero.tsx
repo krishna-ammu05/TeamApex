b39d243ebfa2139608b794ab80d8585cb88b1560
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-12-18 20:44:07 +0530

    Profile added

---
+++ Nextjs/Nextjs_Project/basic_portfolio/src/components/layout/Hero.tsx
@@ -0,0 +1,5 @@
+function Hero(){
+    return <div className="flex justify-center items-center h-screen text-black bg-gradient-to-r from-gradientLeft to-gradientRight">hero</div>
+}
+
+export default Hero;
