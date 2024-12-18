8602ee8842a1b3f07db542a45f699efafa0d9ceb
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-12-18 20:45:11 +0530

    MenuItem added

--- Nextjs/Nextjs_Project/basic_portfolio/src/components/layout/Projects.tsx
+++ Nextjs/Nextjs_Project/basic_portfolio/src/components/layout/Projects.tsx
@@ -1,4 +1,7 @@
 function Projects(){
-    return <div className="flex justify-center items-center bg-gray-800 h-screen text-white">Projects</div>
+    return <div id="Projects" className="bg-gradient-to-r from-gradientLeft to-gradientRight">
+        <img className="w-full h-44" src="/transitions/transition_grey.svg" alt="transition" />
+         <div id="Projects" className="flex justify-center items-center h-screen text-black">Projects</div>
+    </div>
 }
 export default Projects;
