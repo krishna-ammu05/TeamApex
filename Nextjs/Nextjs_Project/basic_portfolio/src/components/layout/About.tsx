1f7e5f7619bb1cd78edc4c552fcafaa1d0fcfa66
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-12-20 18:18:29 +0530

    updated About

--- Nextjs/Nextjs_Project/basic_portfolio/src/components/layout/About.tsx
+++ Nextjs/Nextjs_Project/basic_portfolio/src/components/layout/About.tsx
@@ -4,7 +4,7 @@
 function About() {
     return <div className="bg-slate-800" id="About">
         <img className="w-full h-44" src="/transitions/transition_gradient.svg" alt="transition" />
-        <div className="flex justify-center items-center text-white">
+        <div className="flex justify-center items-center text-black">
             <div className="flex flex-row w-full">
                 <div className="flex desktop:w-1/2 mobile:w-full mobile:p-4 flex-col desktop:p-20">
                     {/* <Summary /> */}
