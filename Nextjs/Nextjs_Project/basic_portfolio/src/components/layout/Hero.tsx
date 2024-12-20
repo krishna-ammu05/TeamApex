2898cc96fee2c0ce9da5ed8f6d8070787202ecdf
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2024-12-20 18:19:34 +0530

    Hero Updated

--- Nextjs/Nextjs_Project/basic_portfolio/src/components/layout/Hero.tsx
+++ Nextjs/Nextjs_Project/basic_portfolio/src/components/layout/Hero.tsx
@@ -1,5 +1,16 @@
+'use-client'
+
+import SocialMedia from "@/static/SocialMedia";
+import Typing from "@/static/typing";
+
 function Hero(){
-    return <div className="flex justify-center items-center h-screen text-black bg-gradient-to-r from-gradientLeft to-gradientRight">hero</div>
+    return <div id='Hero' className="flex items-center min-h-screen px-20 bg-gradient-to-r from-gradientLeft to-gradientRight">
+        <div className="flex flex-row items-center w-full">
+            <div className="flex flex-col z-20 pointer-events-none text-white w-3/5">
+            </div>
+            <img className="w-2/5 object-contain self-center animate-scalepulse" src="/portraitdummy.wetp" alt="Portrait"/>
+        </div>
+    </div>
 }
 
 export default Hero;
