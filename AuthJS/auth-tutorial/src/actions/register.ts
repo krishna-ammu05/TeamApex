99d95f9690c40ac20caf42034a271ff5c7efd4f8
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2025-02-06 22:43:52 +0530

    Create User
    bcryptjs installed

--- AuthJS/auth-tutorial/src/actions/register.ts
+++ AuthJS/auth-tutorial/src/actions/register.ts
@@ -1,5 +1,6 @@
 'use server';
-
+import bcrypt from "bcrypt"
+import { db } from "@/lib/db";
 import * as z from "zod";
 import { revalidatePath, revalidateTag } from "next/cache";
 import {RegisterSchema} from '@/schemas'
@@ -10,7 +11,32 @@ export const register =async (values:z.infer <typeof RegisterSchema>) =>{
     if(!validateFields.success){
         return{error:"Invalid fields!"};
     }
-    return{success:"Email sent"};
+
+    const {email,password,name}= validateFields.data;
+    const hashedPassword = await bcrypt.hash(password,10);
+
+    const existingUser = await db.user.findUnique({
+        where:{
+            email,
+        }
+    });
+
+    if(existingUser){
+        return {
+            error:"Email already in use!"
+        };
+    }
+
+    await db.user.create({
+        data:{
+            name,
+            email,
+            password:hashedPassword,
+        },
+    });
+
+    //send verification token email
+    return {success:"User Created"};
     // revalidatePath()
     // revalidateTag()
 };
